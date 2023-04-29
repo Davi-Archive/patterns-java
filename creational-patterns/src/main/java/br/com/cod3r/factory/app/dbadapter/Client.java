195342d5f76f8b99c91f0b1a77c1a453b74ee14e
d1av <davi4alves@gmail.com>
2023-04-29 03:11:50 -0300

    feat: :sparkles: implemented postgres and iphone factory

--- creational-patterns/src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
@@ -1,8 +1,15 @@
 package br.com.cod3r.factory.app.dbadapter;
 
+import br.com.cod3r.factory.app.dbadapter.db.DB;
+import br.com.cod3r.factory.app.dbadapter.factory.PostgresDBFactory;
+
 public class Client {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+	// DB db = new OracleDBFactory().getDatabase();
+	DB db = new PostgresDBFactory().getDatabase();
+	db.query("SELECT * FROM amigo;");
+	db.update("UPDATE id,name VALUES 'a','b','c';");
 
-	}
+    }
 }
