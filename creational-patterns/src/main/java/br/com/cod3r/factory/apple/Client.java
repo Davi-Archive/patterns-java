cfb6f07097ee456308783b47bb771756128ced7b
d1av <davi4alves@gmail.com>
2023-04-29 03:03:31 -0300

    feat: :sparkles: implemented new iphone factory

--- creational-patterns/src/main/java/br/com/cod3r/factory/apple/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/factory/apple/Client.java
@@ -1,11 +1,22 @@
 package br.com.cod3r.factory.apple;
 
+import br.com.cod3r.factory.apple.factory.IPhoneFactory;
+import br.com.cod3r.factory.apple.model.IPhone;
+import br.com.cod3r.factory.apple.model.IPhone11Pro;
+import br.com.cod3r.factory.apple.model.IPhoneX;
+
 public class Client {
-	
-	public static void main(String[] args) {
-		
-		System.out.println("### Ordering an iPhone X");
-		
-		System.out.println("\n\n### Ordering an iPhone 11 HighEnd");
-	}
+
+    public static void main(String[] args) {
+
+	System.out.println("### Ordering an iPhone X");
+	IPhone iPhone1 = (IPhoneX) IPhoneFactory
+		.orderIPhone("iphonex");
+	System.out.println(iPhone1);
+
+	System.out.println("\n\n### Ordering an iPhone 11 HighEnd");
+	IPhone iPhone2 = (IPhone11Pro) IPhoneFactory
+		.orderIPhone("iphone11pro");
+	System.out.println(iPhone2);
+    }
 }
--- creational-patterns/src/main/java/br/com/cod3r/factory/apple/factory/IPhoneFactory.java
+++ creational-patterns/src/main/java/br/com/cod3r/factory/apple/factory/IPhoneFactory.java
@@ -1,11 +1,39 @@
 package br.com.cod3r.factory.apple.factory;
 
-import br.com.cod3r.abstractfactory.apple.model.iphone.IPhone;
+import br.com.cod3r.factory.apple.model.IPhone;
+import br.com.cod3r.factory.apple.model.IPhone11;
+import br.com.cod3r.factory.apple.model.IPhone11Pro;
+import br.com.cod3r.factory.apple.model.IPhoneX;
 
 public abstract class IPhoneFactory {
 
-	public static IPhone getIphone(String iphoneType) {
-	    
-	    return 
+    public static IPhone orderIPhone(String iphoneType) {
+	IPhone device = null;
+
+	switch (iphoneType) {
+	case "iphone11":
+	    device = new IPhone11();
+	    break;
+	case "iphone11pro":
+	    device = new IPhone11Pro();
+	    break;
+	case "iphonex":
+	    device = new IPhoneX();
+	    break;
+	case "iphonexsmax":
+	    device = new IPhoneX();
+	    break;
+	default:
+	    break;
 	}
+
+	if (device != null) {
+	    device.getHardware();
+	    device.assemble();
+	    device.certificates();
+	    device.pack();
+	}
+
+	return device;
+    }
 }
