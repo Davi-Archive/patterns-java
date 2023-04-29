4e4c529bca6da658795d687180925c6e547f406b
d1av <davi4alves@gmail.com>
2023-04-29 02:50:46 -0300

    feat: :sparkles: patterns to create

---
+++ creational-patterns/src/main/java/br/com/cod3r/factory/apple/factory/IPhoneFactory.java
@@ -0,0 +1,11 @@
+package br.com.cod3r.factory.apple.factory;
+
+import br.com.cod3r.abstractfactory.apple.model.iphone.IPhone;
+
+public abstract class IPhoneFactory {
+
+	public static IPhone getIphone(String iphoneType) {
+	    
+	    return 
+	}
+}
